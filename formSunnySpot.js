20875b8ef508ed90ec8f9128f7c9c75e24440bcf
LucyGIN <lucia@ginetta.net>
2019-07-17 09:33:17 +0200

    made a lot of style guide adds and worked more on Javascript form

--- formSunnySpot.js
+++ formSunnySpot.js
@@ -17,13 +17,48 @@ const activity = {
     'an event!',
   ],
 };
-const location = {
+const location1 = {
   question: 'What do you prefer for location?',
   answer: ['I want to be near the city', 'A bit out of the city'],
 };
+
+const questionArray = [levelSun, crowd, activity, location1];
+// let currentQuestionIndex = 0
+
 const questionSunContainer = document.querySelector('#questionSunContainer');
 const startQuestionsBtn = document.querySelector('#startQuestionsBtn');
 
 startQuestionsBtn.addEventListener('click', () => {
-  alert('form!');
+  //hide the button
+  startQuestionsBtn.classList.add('hidden');
+
+  //create a question box
+  createQuestions(questionArray[0], 0);
 });
+
+function createQuestions(look, index) {
+  const questionBox = document.createElement('div');
+
+  const buttonNext = document.createElement('button');
+  buttonNext.innerHTML = 'next';
+  buttonNext.addEventListener('click', () => {
+    questionBox.classList.add('hidden');
+    createQuestions(questionArray[index + 1], index + 1);
+  });
+
+  const question = document.createElement('h3');
+  question.innerHTML = look.question;
+  questionBox.appendChild(question);
+  //create two answer
+
+  for (let i = 0; i < look.answer.length; i++) {
+    const answerp = document.createElement('p');
+    answerp.innerHTML = look.answer[i];
+    //add answers to question box
+    questionBox.appendChild(answerp);
+  }
+
+  questionBox.appendChild(buttonNext);
+  //add question and answers
+  questionSunContainer.appendChild(questionBox);
+}
